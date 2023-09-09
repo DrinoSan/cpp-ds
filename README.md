6b99a58b3642d2950c4d92af1d0ca9948a7e2a15
DrinoSan <s.becirbegovic@gmail.com>
2023-09-09 07:33:43 +0200

    Linked list

---
+++ README.md
@@ -0,0 +1,2 @@
+# DS implementation in c++
+This Repo is used for the blog. It will/should contain implementations of basic data structures. In the blog I will add explanation.
--- cpp-linked-list/linked-list.cpp
+++ cpp-linked-list/linked-list.cpp
@@ -1,112 +1,147 @@
-#include <cstddef>
 #include <iostream>
 
-class Node {
+class Node
+{
+  private:
+    int32_t data;
+    Node*   next = nullptr;
 
-private:
-  int32_t data;
-  Node *next = nullptr;
+  public:
+    Node( int32_t data ) : data{ data } {}
 
-public:
-  Node(int32_t data) : data{data} {}
-
-  void insertNodeAtEnd(int32_t data) {
-    auto *currNode = this;
-
-    while (true) {
-      if (currNode->next == nullptr) {
-        // Found end of linked list
-        Node *newNode = new Node(data);
-        currNode->next = newNode;
-        return;
-      }
-
-      currNode = currNode->next;
-    }
-  }
-
-  Node *insertNodeAtHead(int32_t data) {
-    Node *node = new Node(data);
-    node->next = this;
-
-    return node;
-  }
-
-  static Node *deleteNode(Node *head, int32_t data) {
-    if (head->data == data) {
-      Node *newHead = head->next;
-      delete head;
-      return newHead;
-    }
-
-    Node *currNode = head;
-    Node *nextNode = head->next;
-
-    while (nextNode->next != nullptr) {
-      if (nextNode->data == data) {
-        currNode->next = nextNode->next;
-        delete nextNode;
-        return nullptr;
-      }
-
-      currNode = nextNode;
-      nextNode = nextNode->next;
-    }
-
-    return nullptr;
-  }
-
-  bool searchNode(Node *node);
-
-  bool isEmpty() {
-    if (this->next == nullptr)
-      return true;
-    return false;
-  }
-
-  int32_t getSize() {
-    if (this->next == nullptr)
-      return 1;
-
-    int32_t len{1};
-    auto *node = this;
-
-    while (node->next != nullptr) {
-      len++;
-      node = node->next;
-    }
-
-    return len;
-  }
-
-  void showList()
-  {
-    Node* currNode = this;
-    int32_t index{0};
-    while( currNode->next != nullptr )
+    void insertNodeAtEnd( int32_t value )
     {
-        std::cout << " -- N(" << index++ << ") Val(" << currNode->data << ") -- " << std::endl;
-        currNode = currNode->next;
-    }
-    std::cout << " -- N(" << index++ << ") Val(" << currNode->data << ") -- " << std::endl;
+        Node* currNode = this;
 
-  }
+        // We iterate over the list to find the last node
+        while ( currNode->next != nullptr )
+        {
+            currNode = currNode->next;
+        }
+
+        // At this point we reached the end
+        Node* newNode  = new Node( value );
+        currNode->next = newNode;
+    }
+
+    Node* insertNodeAtHead( int32_t value )
+    {
+        // We know that we have to add the new node at the beginning
+        // We just need to adjust the next element of the new node to point to
+        // the last head
+        Node* newNode = new Node( value );
+        newNode->next = this;
+
+        return newNode;
+    }
+
+    Node* deleteNode( int32_t value )
+    {
+        // If the head node contains the value we want to delete
+        if ( data == value )
+        {
+            Node* newHead = next;
+            delete this;
+            return newHead;
+        }
+
+        Node* currNode = this;
+
+        while ( currNode->next != nullptr )
+        {
+            if ( currNode->next->data == value )
+            {
+                // We save the address of the node which is to be deleted in tmp
+                Node* tmp = currNode->next;
+
+                // We adjust the next of the current node pointing to the node
+                // after the to be deleted one Basically we just skip the node
+                // in between
+                currNode->next = currNode->next->next;
+                delete tmp;
+                return this;
+            }
+
+            currNode = currNode->next;
+        }
+
+        return this;
+    }
+
+    bool searchNode( int32_t value )
+    {
+        Node* currNode = this;
+        while ( currNode != nullptr )
+        {
+            if ( currNode->data == value )
+            {
+                return true;
+            }
+
+            currNode = currNode->next;
+        }
+
+        return false;
+    }
+
+    bool isEmpty() { return ( next == nullptr ); }
+
+    int32_t getSize()
+    {
+        int32_t len{ 0 };
+        auto*   node = this;
+
+        while ( node != nullptr )
+        {
+            len++;
+            node = node->next;
+        }
+
+        return len;
+    }
+
+    void showList()
+    {
+        Node*   currNode = this;
+        int32_t index{ 0 };
+        while ( currNode != nullptr )
+        {
+            std::cout << " -- N(" << index++ << ") Val(" << currNode->data
+                      << ") -- " << std::endl;
+            currNode = currNode->next;
+        }
+    }
 };
 
-int main() {
+int main()
+{
 
-  Node *head = new Node(0);
+    Node* head = new Node( 0 );
 
-  head->insertNodeAtEnd(1);
-  head->insertNodeAtEnd(2);
-  head->insertNodeAtEnd(3);
-  head = head->insertNodeAtHead(5);
-  Node* res = Node::deleteNode(head, 2);
-  if( res != nullptr)
-    head = res;
+    head->insertNodeAtEnd( 1 );
+    head->insertNodeAtEnd( 2 );
+    head->insertNodeAtEnd( 3 );
+    head = head->insertNodeAtHead( 5 );
 
-  head->showList();
-  std::cout << "Size: " << head->getSize() << std::endl;
+    std::cout << "Original List:" << std::endl;
+    head->showList();
 
-  return 0;
+    // Delete a node
+    head = head->deleteNode( 2 );
+    std::cout << "After deleting node with value 2:" << std::endl;
+    head->showList();
+
+    int32_t searchValue = 2;
+    if ( head->searchNode( searchValue ) )
+    {
+        std::cout << "Node with value " << searchValue << " found."
+                  << std::endl;
+    }
+    else
+    {
+        std::cout << "Node with value " << searchValue << " not found."
+                  << std::endl;
+    }
+
+    return 0;
 }
